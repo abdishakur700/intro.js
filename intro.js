ae443a9168f6af35663c1990b1044f66a112757b
Abdishakur <dibaacade142@gmail.com>
2026-06-10 11:21:11 +0300

    Added intro.js assignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+
+
+let name = "Abdishakur";
+let age = 20;
+let favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
